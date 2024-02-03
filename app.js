a13e984280fd9f408cb76d4ea4dc94128c2b570c
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-02-03 21:36:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var input = document.querySelector(".input");
+const btn = document.querySelector(".addbtn");
+const addedlist = document.querySelector(".addedlist");
+
+btn.addEventListener(Click, ()=>{
+    if(input.value != ""){
+    
+        alert("Error");
+    }
+    else{
+        alert("Error");
+    }
+});
